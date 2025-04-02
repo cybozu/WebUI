b403b9bb28fadd3edd796ba3a7d8d3a38a07f8b7
Takuto NAKAMURA (Kyome) <kyomesuke@gmail.com>
2025-04-02 19:10:23 +0900

    Added tests for WebViewProxy property binding.

--- Tests/WebUITests/Mock.swift
+++ Tests/WebUITests/Mock.swift
@@ -10,6 +10,29 @@ final class EnhancedWKWebViewMock: EnhancedWKWebView {
     private(set) var goForwardCalled = false
     private(set) var javaScriptString: String?
 
+    private var _title: String?
+    override var title: String? { _title }
+
+    private var _url: URL?
+    override var url: URL? { _url }
+
+    private var _isLoading: Bool = false
+    override var isLoading: Bool { _isLoading }
+
+    private var _estimatedProgress: Double = .zero
+    override var estimatedProgress: Double { _estimatedProgress }
+
+    private var _canGoBack: Bool = false
+    override var canGoBack: Bool { _canGoBack }
+
+    private var _canGoForward: Bool = false
+    override var canGoForward: Bool { _canGoForward }
+
+    #if canImport(UIKit)
+    private var _contentSize: CGSize = .zero
+    private var _contentOffset: CGPoint = .zero
+    #endif
+
     override func load(_ request: URLRequest) -> WKNavigation? {
         loadedRequest = request
         return nil
@@ -60,3 +83,47 @@ final class UIDelegateMock: NSObject, WKUIDelegate {}
 final class NavigationDelegateMock: NSObject, WKNavigationDelegate {}
 
 final class WebViewConfigurationMock: WKWebViewConfiguration {}
+
+extension EnhancedWKWebViewMock {
+    convenience init(title: String) {
+        self.init()
+        _title = title
+    }
+
+    convenience init(url: URL) {
+        self.init()
+        _url = url
+    }
+
+    convenience init(isLoading: Bool) {
+        self.init()
+        _isLoading = isLoading
+    }
+
+    convenience init(estimatedProgress: Double) {
+        self.init()
+        _estimatedProgress = estimatedProgress
+    }
+
+    convenience init(canGoBack: Bool) {
+        self.init()
+        _canGoBack = canGoBack
+    }
+
+    convenience init(canGoForward: Bool) {
+        self.init()
+        _canGoForward = canGoForward
+    }
+
+    #if canImport(UIKit)
+    convenience init(contentSize: CGSize) {
+        self.init()
+        scrollView.contentSize = contentSize
+    }
+
+    convenience init(contentOffset: CGPoint) {
+        self.init()
+        scrollView.contentOffset = contentOffset
+    }
+    #endif
+}
--- Tests/WebUITests/WebViewProxyTests.swift
+++ Tests/WebUITests/WebViewProxyTests.swift
@@ -5,6 +5,96 @@ import Testing
 
 struct WebViewProxyTests {
     @MainActor @Test
+    func property_binding_title() async throws {
+        let sut = WebViewProxy()
+        let webViewMock = Remakeable {
+            EnhancedWKWebViewMock(title: "dummy") as EnhancedWKWebView
+        }
+        sut.setUp(webViewMock)
+        try await Task.sleep(for: .seconds(0.1))
+        #expect(sut.title == "dummy")
+    }
+
+    @MainActor @Test
+    func property_binding_url() async throws {
+        let sut = WebViewProxy()
+        let webViewMock = Remakeable {
+            EnhancedWKWebViewMock(url: URL(string: "https://www.example.com")!) as EnhancedWKWebView
+        }
+        sut.setUp(webViewMock)
+        try await Task.sleep(for: .seconds(0.1))
+        #expect(sut.url == URL(string: "https://www.example.com")!)
+    }
+
+    @MainActor @Test
+    func property_binding_isLoading() async throws {
+        let sut = WebViewProxy()
+        let webViewMock = Remakeable {
+            EnhancedWKWebViewMock(isLoading: true) as EnhancedWKWebView
+        }
+        sut.setUp(webViewMock)
+        try await Task.sleep(for: .seconds(0.1))
+        #expect(sut.isLoading)
+    }
+
+    @MainActor @Test
+    func property_binding_estimatedProgress() async throws {
+        let sut = WebViewProxy()
+        let webViewMock = Remakeable {
+            EnhancedWKWebViewMock(estimatedProgress: 0.5) as EnhancedWKWebView
+        }
+        sut.setUp(webViewMock)
+        try await Task.sleep(for: .seconds(0.1))
+        #expect(sut.estimatedProgress == 0.5)
+    }
+
+    @MainActor @Test
+    func property_binding_canGoBack() async throws {
+        let sut = WebViewProxy()
+        let webViewMock = Remakeable {
+            EnhancedWKWebViewMock(canGoBack: true) as EnhancedWKWebView
+        }
+        sut.setUp(webViewMock)
+        try await Task.sleep(for: .seconds(0.1))
+        #expect(sut.canGoBack)
+    }
+
+    @MainActor @Test
+    func property_binding_canGoForward() async throws {
+        let sut = WebViewProxy()
+        let webViewMock = Remakeable {
+            EnhancedWKWebViewMock(canGoForward: true) as EnhancedWKWebView
+        }
+        sut.setUp(webViewMock)
+        try await Task.sleep(for: .seconds(0.1))
+        #expect(sut.canGoForward)
+    }
+
+    #if canImport(UIKit)
+    @MainActor @Test
+    func property_binding_contentSize() async throws {
+        let sut = WebViewProxy()
+        let webViewMock = Remakeable {
+            EnhancedWKWebViewMock(contentSize: .init(width: 50, height: 50)) as EnhancedWKWebView
+        }
+        sut.setUp(webViewMock)
+        try await Task.sleep(for: .seconds(0.1))
+        #expect(sut.contentSize.equalTo(.init(width: 50, height: 50)))
+    }
+
+    @MainActor @Test
+    func property_binding_contentOffset() async throws {
+        let sut = WebViewProxy()
+        let webViewMock = Remakeable {
+            EnhancedWKWebViewMock(contentOffset: .init(x: 50, y: 50)) as EnhancedWKWebView
+        }
+        sut.setUp(webViewMock)
+        try await Task.sleep(for: .seconds(0.1))
+        #expect(sut.contentOffset.equalTo(.init(x: 50, y: 50)))
+    }
+    #endif
+
+    @MainActor @Test
     func load_the_specified_URLRequest() {
         let sut = WebViewProxy()
         let webViewMock = Remakeable {
