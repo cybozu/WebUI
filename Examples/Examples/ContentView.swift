738511c750edba25ae1e12ff9d40f3094dc175db
elmetal <nm7.pupu@gmail.com>
2025-04-22 09:07:07 +0900

    fix indentation
    Co-authored-by: b1ackturtle <ichibon0@gmail.com>

--- Examples/Examples/ContentView.swift
+++ Examples/Examples/ContentView.swift
@@ -128,18 +128,18 @@ private extension WebViewProxy {
 extension PDFDocument: @retroactive Transferable {
     public static var transferRepresentation: some TransferRepresentation {
         DataRepresentation(contentType: .pdf) { pdf in
-                if let data = pdf.dataRepresentation() {
-                    return data
-                } else {
-                    return Data()
-                }
-            } importing: { data in
-                if let pdf = PDFDocument(data: data) {
-                    return pdf
-                } else {
-                    return PDFDocument()
-                }
+            if let data = pdf.dataRepresentation() {
+                return data
+            } else {
+                return Data()
             }
+        } importing: { data in
+            if let pdf = PDFDocument(data: data) {
+                return pdf
+            } else {
+                return PDFDocument()
+            }
+        }
 
         DataRepresentation(exportedContentType: .pdf) { pdf in
             if let data = pdf.dataRepresentation() {
